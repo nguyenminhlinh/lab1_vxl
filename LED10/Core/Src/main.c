ca9254a8738e6eace89b78e730c9f7216e70aa42
nguyenminhlinh <linhh.nguyenminh@gmail.com>
2022-10-02 09:53:09 +0700

    add b10

---
+++ LED10/Core/Src/main.c
@@ -0,0 +1,277 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+  void clearAllClock ()
+    {
+  	  HAL_GPIO_WritePin(P4_GPIO_Port, P4_Pin, 1);
+  	  HAL_GPIO_WritePin(P5_GPIO_Port, P5_Pin, 1);
+  	  HAL_GPIO_WritePin(P6_GPIO_Port, P6_Pin, 1);
+  	  HAL_GPIO_WritePin(P7_GPIO_Port, P7_Pin, 1);
+  	  HAL_GPIO_WritePin(P8_GPIO_Port, P8_Pin, 1);
+  	  HAL_GPIO_WritePin(P9_GPIO_Port, P9_Pin, 1);
+  	  HAL_GPIO_WritePin(P10_GPIO_Port, P10_Pin, 1);
+  	  HAL_GPIO_WritePin(P11_GPIO_Port, P11_Pin, 1);
+  	  HAL_GPIO_WritePin(P12_GPIO_Port, P12_Pin, 1);
+  	  HAL_GPIO_WritePin(P13_GPIO_Port, P13_Pin, 1);
+  	  HAL_GPIO_WritePin(P14_GPIO_Port, P14_Pin, 1);
+  	  HAL_GPIO_WritePin(P15_GPIO_Port, P15_Pin, 1);
+    }
+  void Clock(int count_hour,int count_minute,int count_second)
+    {
+  	  switch(count_second)
+  	  	  {
+  	  		  case 1:{HAL_GPIO_WritePin(P4_GPIO_Port, P4_Pin, 0);break;}
+  	  		  case 2:{HAL_GPIO_WritePin(P5_GPIO_Port, P5_Pin, 0);break;}
+  	  		  case 3:{HAL_GPIO_WritePin(P6_GPIO_Port, P6_Pin, 0);break;}
+  	  		  case 4:{HAL_GPIO_WritePin(P7_GPIO_Port, P7_Pin, 0);break;}
+  	  		  case 5:{HAL_GPIO_WritePin(P8_GPIO_Port, P8_Pin, 0);break;}
+  	  		  case 6:{HAL_GPIO_WritePin(P9_GPIO_Port, P9_Pin, 0);break;}
+  	  		  case 7:{HAL_GPIO_WritePin(P10_GPIO_Port, P10_Pin, 0);break;}
+  	  		  case 8:{HAL_GPIO_WritePin(P11_GPIO_Port,P11_Pin, 0);break;}
+  	  		  case 9:{HAL_GPIO_WritePin(P12_GPIO_Port, P12_Pin, 0);break;}
+  	  		  case 10:{HAL_GPIO_WritePin(P13_GPIO_Port, P13_Pin, 0);break; }
+  	  		  case 11:{HAL_GPIO_WritePin(P14_GPIO_Port, P14_Pin, 0);break;}
+  	  		case 12:{HAL_GPIO_WritePin(P15_GPIO_Port, P15_Pin, 0);break;
+  	  		default: break;}
+  	  	  }
+  	  	  	if(count_minute>=5&&count_minute<10){HAL_GPIO_WritePin(P4_GPIO_Port, P4_Pin, 0);}
+  	  	  	if(count_minute>=10&&count_minute<15){HAL_GPIO_WritePin(P5_GPIO_Port, P5_Pin, 0);}
+  	  	  	if(count_minute>=15&&count_minute<20){HAL_GPIO_WritePin(P6_GPIO_Port, P6_Pin, 0);}
+  	  	  	if(count_minute>=20&&count_minute<25){HAL_GPIO_WritePin(P7_GPIO_Port, P7_Pin, 0);}
+  	  	  	if(count_minute>=25&&count_minute<30){HAL_GPIO_WritePin(P8_GPIO_Port, P8_Pin, 0);}
+  	  	  	if(count_minute>=30&&count_minute<35){HAL_GPIO_WritePin(P9_GPIO_Port, P9_Pin, 0);}
+  	  	  	if(count_minute>=35&&count_minute<40){HAL_GPIO_WritePin(P10_GPIO_Port, P10_Pin, 0);}
+  	  	  	if(count_minute>=40&&count_minute<45){HAL_GPIO_WritePin(P11_GPIO_Port,P11_Pin, 0); }
+  	  	  	if(count_minute>=45&&count_minute<50){HAL_GPIO_WritePin(P12_GPIO_Port, P12_Pin, 0); }
+  	  	  	if(count_minute>=50&&count_minute<55){HAL_GPIO_WritePin(P13_GPIO_Port, P13_Pin, 0);}
+  	  	  	if(count_minute>=55&&count_minute<60){HAL_GPIO_WritePin(P14_GPIO_Port, P14_Pin, 0);}
+  	  	  	if(count_minute==60||count_minute<5){HAL_GPIO_WritePin(P15_GPIO_Port, P15_Pin, 0);}
+  	  	  switch(count_hour)
+  	  	  {
+  	  		  case 1:{HAL_GPIO_WritePin(P4_GPIO_Port, P4_Pin, 0);break;}
+  	  		  case 2:{HAL_GPIO_WritePin(P5_GPIO_Port, P5_Pin, 0);break; }
+  	  		  case 3:{HAL_GPIO_WritePin(P6_GPIO_Port, P6_Pin, 0);break;}
+  	  		  case 4:{HAL_GPIO_WritePin(P7_GPIO_Port, P7_Pin, 0);break;}
+  	  		  case 5:{HAL_GPIO_WritePin(P8_GPIO_Port, P8_Pin, 0);break;}
+  	  		  case 6:{HAL_GPIO_WritePin(P9_GPIO_Port, P9_Pin, 0);break;}
+  	  		  case 7:{HAL_GPIO_WritePin(P10_GPIO_Port, P10_Pin, 0);break;}
+  	  		  case 8:{HAL_GPIO_WritePin(P11_GPIO_Port,P11_Pin, 0);break;}
+  	  		  case 9:{HAL_GPIO_WritePin(P12_GPIO_Port, P12_Pin, 0);break; }
+  	  		  case 10:{HAL_GPIO_WritePin(P13_GPIO_Port, P13_Pin, 0);break;}
+  	  		  case 11:{HAL_GPIO_WritePin(P14_GPIO_Port, P14_Pin, 0);break; }
+  	  		  case 12:{HAL_GPIO_WritePin(P15_GPIO_Port, P15_Pin, 0);break;}
+  	  		  default: break;
+  	  	  }
+    }
+  int count_second=12;
+  int count_minute=60;
+  int count_hour=12;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  clearAllClock ();
+	  Clock(count_hour, count_minute, count_second);
+	  if(count_second==11){count_minute++;}
+	  if(count_second==11 && count_minute==60){count_hour++;}
+	  count_second++;
+	  if(count_second>12){count_second=1;}
+	  if(count_minute>60){count_minute=1;}
+	  if(count_hour>12){count_hour=1;}
+	  HAL_Delay(5000);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, P4_Pin|P5_Pin|P6_Pin|P7_Pin
+                          |P8_Pin|P9_Pin|P10_Pin|P11_Pin
+                          |P12_Pin|P13_Pin|P14_Pin|P15_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : P4_Pin P5_Pin P6_Pin P7_Pin
+                           P8_Pin P9_Pin P10_Pin P11_Pin
+                           P12_Pin P13_Pin P14_Pin P15_Pin */
+  GPIO_InitStruct.Pin = P4_Pin|P5_Pin|P6_Pin|P7_Pin
+                          |P8_Pin|P9_Pin|P10_Pin|P11_Pin
+                          |P12_Pin|P13_Pin|P14_Pin|P15_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
